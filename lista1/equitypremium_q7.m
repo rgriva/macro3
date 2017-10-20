30bea2eda27ac571e95861fa4fd7dfb14e020696
Raul <raul.guarini@gmail.com>
2017-10-20 18:36:29 -0200

    Adds burn parameter to Problem 7 and updates pdf report

--- lista1/equitypremium_q7.m
+++ lista1/equitypremium_q7.m
@@ -14,14 +14,20 @@ pop = data(:,4);
 selic = data(:,5);
 preco = data(:,7);
 
+% Escolhendo de quando queremos comecar o exercicio numerico.
+% Para que o ano base seja 1994, burn = 0.
+
+burn = 0;
+
 % Dividindo o consumo nominal em precos correntes pelo indice de precos
 % e pela populacao
 consumo_pc = consumo_bruto./(preco.*pop);
+consumo_pc = consumo_pc(1+burn:end);
 
 % Taxa de crescimento do consumo real per capta
 gth_rate_c = (consumo_pc(2:end) - consumo_pc(1:end-1))./consumo_pc(1:end-1);
 
-ibov_retorno = (ibov(2:end) - ibov(1:end-1))./ibov(1:end-1);
+ibov_retorno = (ibov(2+burn:end) - ibov(1+burn:end-1))./ibov(1+burn:end-1);
 
 %% Calibrando o modelo
 % Seguindo a calibracao de Mehra & Prescott (1985)
@@ -30,21 +36,21 @@ mu = mean(gth_rate_c);
 delta = std(gth_rate_c);
 acf = autocorr(gth_rate_c,1);
 phi = 0.5 * (1 + acf(2));
-nbeta = 500;
-nsigma = 500;
-beta_grid = linspace(0.1,1,nbeta); 
-beta_grid(nbeta) = 0.99;
+nbeta = 200;
+nsigma = 200;
+beta_grid = linspace(0.9,1,nbeta); 
+beta_grid(nbeta) = 0.9999;
 sigma_grid = linspace(1,10,nsigma);
 
 x1 = 1 + mu + delta;
-x2 = 1 + mu -delta;
+x2 = 1 + mu - delta;
 x = [x1, x2];
 
 % Ha 20 observacoes de retorno pois perdemos uma observacao ao calcular
 % os retornos a partir dos indices. Por isso, vamos desconsiderar a SELIC
 % de 1994, ano base.
 
-diff_retorno = ibov_retorno - selic(2:end);
+diff_retorno = ibov_retorno - selic(2+burn:end);
 
 % Premio de risco da economia
 equity_premium = mean(diff_retorno);
@@ -72,9 +78,9 @@ lambda2 = x2.^(-sigma);
 % Retornos livres de risco gerados pelo modelo, para cada combinacao
 % de beta (linha) e sigma (coluna).
 
-p1f = (P(1,1).*lambda1 + P(1,2)*lambda2).*beta;
+p1f = (P(1,1)*lambda1 + P(1,2)*lambda2).*beta;
 R1f = 1./p1f - 1;
-p2f = (P(2,1).*lambda1 + P(2,2)*lambda2).*beta;
+p2f = (P(2,1)*lambda1 + P(2,2)*lambda2).*beta;
 R2f = 1./p2f - 1;
 
 % Usando a distribuicao invariante da cadeia de Markov
@@ -93,7 +99,7 @@ for k = 1:nbeta
             for j = 1:length(x)
                 A(i,j) = beta_grid(k)*(x(j)^(1-sigma_grid(m))*P(i,j));
             end
-            b(i) = beta_grid(k)*sum(A(i,:));
+            b(i) = sum(A(i,:));
         end
         q = (eye(size(A)) - A)\b;
         Re11 = (q(1) + 1)*x(1)/q(1) - 1;
@@ -106,6 +112,7 @@ for k = 1:nbeta
         Re2 = P(2,:)*R(2,:)';
      
         Re(k,m) = inv_dist(1)*Re1 + inv_dist(2)*Re2;
+        
     end
 end
 toc
@@ -113,15 +120,17 @@ toc
 %% Analisando a simulacao
 equity_premium_simulado = Re - Rf;
 
-dist = abs(equity_premium_simulado - equity_premium*ones(size(Re)));
-[a,b] = find(dist == min(min(dist)));
-beta_optimal = beta_grid(a)
-sigma_optimal = sigma_grid(b)
+dist = abs((equity_premium_simulado - equity_premium*ones(size(Rf))));
+[a,b] = find(dist == min(dist(:)));
+beta_optimal = beta_grid(b)
+sigma_optimal = sigma_grid(a)
 
 %% Plotando
 figure('DefaultAxesFontSize',16); hold on
-waterfall(beta_grid,sigma_grid,dist)
+plot = surf(beta_grid,sigma_grid,dist);
+grid on;
+set(plot,'LineStyle','none')
 title('Equity Premium')
 xlabel('Beta')
-ylabel('Gama')
+ylabel('Sigma')
 zlabel('Modulo da Diferenca')
